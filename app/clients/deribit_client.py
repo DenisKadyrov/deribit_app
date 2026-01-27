d6d1973fc17a39fc094f30090a20df1eb1705ba9
Denis <kripmrx@gmail.com>
2026-01-27 17:10:39 +0500

    feat(Client): added aiohttp session property

--- app/clients/deribit_client.py
+++ app/clients/deribit_client.py
@@ -1,28 +1,18 @@
-import aiohttp
-import asyncio
+from aiohttp import ClientSession
 
-from core.config import settings
-from core.database import AsyncSessionLocal 
+from app.core.config import settings
+from app.core.database import AsyncSessionLocal 
 
 
 class DeribitClient:
-  def __init__(self, base_url: str):
+  def __init__(self, base_url: str, session: ClientSession):
     self.base_url = base_url
+    self.session = session
 
   async def get_index_price(self, currency: str) -> float:
     url = f"{self.base_url}/public/get_index_price"
     params = {"index_name": f"{currency.lower()}_usd"}
 
-    async with aiohttp.ClientSession() as session:
-      async with session.get(url, params=params) as resp:
-        data = await resp.json()
-        return data["result"]["index_price"]
-
-
-async def main():
-  deribit_client = DeribitClient(settings.DERIBIT_URL)
-  res = await deribit_client.get_index_price("eth")
-  print(res)
-
-if __name__ == "__main__":
-    asyncio.run(main())
+    async with self.session.get(url, params=params) as resp:
+      data = await resp.json()
+      return data["result"]["index_price"]
