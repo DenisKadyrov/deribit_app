9e0c9209f932bc05621c128c490a8af953fca669
Denis <kripmrx@gmail.com>
2026-01-28 00:14:53 +0500

    feat: PriceOut schema

---
+++ app/schemas/price.py
@@ -0,0 +1,12 @@
+from decimal import Decimal
+from pydantic import BaseModel
+from app.models.price import Price
+
+class PriceOut(BaseModel):
+    ticker: str
+    price: Decimal
+    ts: int
+
+    @classmethod
+    def from_orm(cls, price: Price) -> "PriceOut":
+        return cls(ticker=price.ticker, price=price.price, ts=price.ts)
