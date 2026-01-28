43a029e7b9ffc9d627e05e3a26224427c5ecc1f0
Denis <kripmrx@gmail.com>
2026-01-28 16:31:34 +0500

    refactor: add types

--- app/repositories/price.py
+++ app/repositories/price.py
@@ -5,31 +5,40 @@ from app.models.price import Price
 
 
 class PriceRepository:
-    def __init__(self, session: AsyncSession):
+    def __init__(self, session: AsyncSession) -> None:
         self.session = session
 
-    async def add(self, price: Price):
+    async def add(self, price: Price) -> None:
         self.session.add(price)
         await self.session.commit()
 
-    async def get_all(self, ticker: str):
-        stmt = select(Price).where(Price.ticker == ticker)
-        result = await self.session.execute(stmt)
-        return result.scalars().all()
-
-    async def get_last(self, ticker: str):
+    async def get_all(self, ticker: str) -> list[Price]:
         stmt = (
             select(Price)
             .where(Price.ticker == ticker)
-            .order_by(desc(Price.timestamp))
+            .order_by(Price.ts.asc())
+        )
+        result = await self.session.execute(stmt)
+        return result.scalars().all()
+
+    async def get_last(self, ticker: str) -> Price | None:
+        stmt = (
+            select(Price)
+            .where(Price.ticker == ticker)
+            .order_by(desc(Price.ts))
             .limit(1)
         )
         result = await self.session.execute(stmt)
         return result.scalar_one_or_none()
 
-    async def get_by_date(self, ticker: str, ts_from: int, ts_to: int):
-        stmt = select(Price).where(
-            Price.ticker == ticker, Price.timestamp.between(ts_from, ts_to)
+    async def get_by_date(self, ticker: str, ts_from: int, ts_to: int) -> list[Price]:
+        stmt = (
+            select(Price)
+            .where(
+                Price.ticker == ticker,
+                Price.ts.between(ts_from, ts_to),
+            )
+            .order_by(Price.ts.asc())
         )
         result = await self.session.execute(stmt)
         return result.scalars().all()
