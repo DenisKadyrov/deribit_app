18295d1ae4f7886e604da07c42bd2858294e9535
Denis <kripmrx@gmail.com>
2026-01-28 21:49:58 +0500

    refactor: formatting

--- app/core/database.py
+++ app/core/database.py
@@ -4,7 +4,9 @@ from app.core.config import settings
 
 engine = create_async_engine(settings.DATABASE_URL, echo=True)
 
-AsyncSessionLocal = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+AsyncSessionLocal = async_sessionmaker(
+    engine, class_=AsyncSession, expire_on_commit=False
+)
 
 
 async def get_session() -> AsyncSession:
